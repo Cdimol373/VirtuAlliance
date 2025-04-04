0e0d5e543d48db0a1973218ba2bca27a59607268
IngridDiazRisso <idiazris@uoc.edu>
2025-04-04 20:54:40 +0200

    Subiendo cambios en los objetos

--- Assets/Minijuegos/Catastrofe/Scripts/AjustarCamara.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AjustarCamara : MonoBehaviour
-{
-    void Start()
-    {
-        Camera.main.orthographicSize = Screen.height / (float)Screen.width * 5f;
-    }
-}
